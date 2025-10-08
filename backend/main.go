860e7d227933ac327ae71e76d837fca65845bdfe
lottola <lottolaaa@gmail.com>
2025-10-08 22:51:17 +0700

    Fix CORS AllowedOrigin

--- backend/main.go
+++ backend/main.go
@@ -43,7 +43,7 @@ func main() {
 
 	router := chi.NewRouter()
 router.Use(cors.Handler(cors.Options{
-    AllowedOrigins:   []string{"https://taskcosmos-pro.netlify.app/"},
+    AllowedOrigins:   []string{"https://taskcosmos-pro.netlify.app"},
     AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
     AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
     ExposedHeaders:   []string{"Link"},
